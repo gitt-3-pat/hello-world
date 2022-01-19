c9b4335a0d4f6c5bdf579ec7b054f808ba5498d3
ailwix79 <90765814+ailwix79@users.noreply.github.com>
2022-01-19 10:53:58 +0100

    CAMBIOS DESDE INTELLIJ A NUEVA BRANCH

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,11 +4,12 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "Modificacion Practica 0. Comprobar Funcionamiento");
+	    System.out.println( "Modificacion Practica 0. Comprobar Funcionamiento"); //Desde bloc de notas
+        System.out.println( "Modificacion realizada desde IntellIj"); //Desde IntellIJ
     }
 }
