f1a507f6d97cb8c8dfda2d9e3863a0c88d49f4a1
ailwix79 <90765814+ailwix79@users.noreply.github.com>
2022-01-19 10:43:12 +0100

    MODIFICACION PRACTICA 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println( "Modificacion Practica 0. Comprobar Funcionamiento");
     }
 }
