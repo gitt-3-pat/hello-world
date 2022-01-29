1746aa8c2038d960d5bacac51c17ffb332c1818b
joserra404 <90766967+joserra404@users.noreply.github.com>
2022-01-29 19:42:33 +0100

    Mensaje nuevo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,11 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "I'm testing git!" );
     }
 }
