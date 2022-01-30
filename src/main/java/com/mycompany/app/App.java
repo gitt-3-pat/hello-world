1ded0272f1e2a6a903ecc0759e4ae84c45a7d9ba
joserra404 <90766967+joserra404@users.noreply.github.com>
2022-01-30 17:04:23 +0100

    Rama2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "I'm testing git!" );
-        System.out.println( "This line should appear in branch 1" );
+        System.out.println( "This is branch #2 and this branch will be merged to main" );
     }
 }
